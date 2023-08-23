345577378daa2bc45b48f829420a2131ced8a2bf
Kyle Peyton <kyle.peyton@acu.edu.au>
2023-08-23 10:32:34 +1000

    Add procfile, php, composer

---
+++ territory_api.php
@@ -0,0 +1,31 @@
+<?php
+
+// Read the dataset
+$data = array_map('str_getcsv', file('territory_data.csv'));
+$headers = array_shift($data);
+$csv = [];
+foreach ($data as $row) {
+    $csv[] = array_combine($headers, $row);
+}
+
+// Fetch parameters from the request
+$locality = strtoupper($_GET['locality'] ?? '');
+$state = strtoupper($_GET['state'] ?? '');
+$postcode = $_GET['postcode'] ?? '';
+
+// Search the dataset
+$response = [];
+foreach ($csv as $row) {
+    if ($row['locality'] == $locality && $row['state'] == $state && $row['postcode'] == $postcode) {
+        $response[] = $row;
+    }
+}
+
+// Return the result
+if (empty($response)) {
+    echo json_encode(['error' => 'Invalid combination of locality, state, and postcode.']);
+} else {
+    echo json_encode($response);
+}
+
+?>
